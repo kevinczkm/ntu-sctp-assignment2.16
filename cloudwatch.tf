69b69a4ff456860d2bfdaf158f7a56019efec212
kevinczkm <136417850+kevinczkm@users.noreply.github.com>
2025-06-19 21:17:58 +0800

    Update cloudwatch.tf
    add in sns topic

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -15,5 +15,17 @@ resource "aws_cloudwatch_metric_alarm" "kvin_info_count_breach" {
   treat_missing_data        = "missing"
   actions_enabled           = true
 
+  alarm_actions = [aws_sns_topic.alerts.arn]
+
   alarm_description         = "Alarm triggers if info-count > 10 within 1 minute"
+}
+
+resource "aws_sns_topic" "alerts" {
+  name = "my-sns-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_alert" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "your-email@example.com"  # You'll get a confirmation email
 }
